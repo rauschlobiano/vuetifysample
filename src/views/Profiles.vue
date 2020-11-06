f5bd36ffcdb5e7a21ee39c15615a01e1ed54046c
Ron <ron@valhallaonlineservices.com>
2020-11-06 22:35:41 +0800

    Profiles gender and type columns

--- src/views/Profiles.vue
+++ src/views/Profiles.vue
@@ -4,6 +4,97 @@
         <v-col class="ml-4">
             <div class="text-h5">Profiles Management</div>
         </v-col>
+        <v-col>
+        <v-progress-circular v-if="loading"
+            indeterminate
+            color="primary"
+        ></v-progress-circular>
+ 
+                <v-dialog
+                v-model="dialog"
+                persistent
+                max-width="600px"
+                >
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn
+                    color="primary"
+                    dark
+                    v-bind="attrs"
+                    v-on="on"
+                    >
+                    Open Dialog
+                    </v-btn>
+                </template>
+                <v-card>
+                    <v-card-title>
+                    <span class="headline">Profile Details</span>
+                    </v-card-title>
+                    <v-card-text>
+                    <v-container>
+                        <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field label="Legal first name*" required></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                            <v-text-field label="Legal last name*" hint="example of persistent helper text" persistent-hint
+                            required
+                            ></v-text-field>
+                        </v-col>
+                        <v-col cols="12"> <v-text-field label="Email*" required ></v-text-field>
+                        </v-col>
+                        <v-col cols="12">
+                            <v-text-field label="Password*"  type="password"
+                            required
+                            ></v-text-field>
+                        </v-col>
+                        <v-col
+                            cols="12"
+                            sm="6"
+                        >
+                            <v-select
+                            :items="['0-17', '18-29', '30-54', '54+']"
+                            label="Age*"
+                            required
+                            ></v-select>
+                        </v-col>
+                        <v-col
+                            cols="12"
+                            sm="6"
+                        >
+                            <v-autocomplete
+                            :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                            label="Interests"
+                            multiple
+                            ></v-autocomplete>
+                        </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                    </v-card-text>
+                    <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="dialog = false"
+                    >
+                        Close
+                    </v-btn>
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="dialog = false"
+                    >
+                        Save
+                    </v-btn>
+                    </v-card-actions>
+                </v-card>
+                </v-dialog>
+            
+        </v-col>
     </v-row>
     
     <v-row>
@@ -14,7 +105,7 @@
             :headers="profileHeader" 
             :items="profiles" 
             :items-per-page="20" 
-            @click:row="clickedRow"
+            @click:row="clickedRow"            
             class="elevation-1"></v-data-table>
         </v-col>
     </v-row>
@@ -39,15 +130,23 @@ export default {
     methods: {
         clickedRow: (e) => {
             console.log(e);
-        }
+            this.currentRow = e;
+        },
+       
+
     },
     mounted() {
+        this.loading = true;
           axios
             .get('http://localhost:8000/api/profiles')
             .then(response => (this.profiles = response.data))
+            this.loading = false;
     },
     data() {
         return {
+            loading: true,
+            dialog: false,
+            currentRow: [],
             profileHeader: [
                 {text: 'Profile ID', align: 'start', sortable: true, value: 'profid'},
                 {text: 'Account Name', align: 'start', sortable: true, value: 'accountname'},
@@ -56,50 +155,14 @@ export default {
                 {text: 'Type', align: 'start', sortable: true, value: 'proftype.proftypedescrip'},
             ],
             profiles: [],
-            headers: [{
-                    text: 'Dessert (100g serving)',
-                    align: 'start',
-                    sortable: false,
-                    value: 'name',
-                },
-                {
-                    text: 'Calories',
-                    value: 'calories'
-                },
-                {
-                    text: 'Fat (g)',
-                    value: 'fat'
-                },
-                {
-                    text: 'Carbs (g)',
-                    value: 'carbs'
-                },
-                {
-                    text: 'Protein (g)',
-                    value: 'protein'
-                },
-                {
-                    text: 'Iron (%)',
-                    value: 'iron'
-                },
-            ],
-            desserts: [{
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: '1%',
-                },                
-            ],
-
         }
     },
 }
 </script>
 
-<style scoped>
-.v-data-table td {
+<style>
+.v-data-table>.v-data-table__wrapper>table>tbody>tr>td 
+{
   font-size: 10px;
 }
 </style>
