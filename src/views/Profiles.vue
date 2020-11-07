967ac0e9f476d40addf291118654a2431a0b5c4f
Ron Jones <rondfirst@gmail.com>
2020-11-07 20:17:36 +0800

    Dialog in profiles

--- src/views/Profiles.vue
+++ src/views/Profiles.vue
@@ -5,121 +5,82 @@
             <div class="text-h5">Profiles Management</div>
         </v-col>
         <v-col>
-        <v-progress-circular v-if="loading"
-            indeterminate
-            color="primary"
-        ></v-progress-circular>
- 
-                <v-dialog
-                v-model="dialog"
-                persistent
-                max-width="600px"
-                >
-                <template v-slot:activator="{ on, attrs }">
-                    <v-btn
-                    color="primary"
-                    dark
-                    v-bind="attrs"
-                    v-on="on"
-                    >
-                    Open Dialog
-                    </v-btn>
-                </template>
-                <v-card>
-                    <v-card-title>
-                    <span class="headline">Profile Details</span>
-                    </v-card-title>
-                    <v-card-text>
-                    <v-container>
-                        <v-row>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-text-field label="Legal first name*" required></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="4">
-                            <v-text-field label="Legal last name*" hint="example of persistent helper text" persistent-hint
-                            required
-                            ></v-text-field>
-                        </v-col>
-                        <v-col cols="12"> <v-text-field label="Email*" required ></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                            <v-text-field label="Password*"  type="password"
-                            required
-                            ></v-text-field>
-                        </v-col>
-                        <v-col
-                            cols="12"
-                            sm="6"
-                        >
-                            <v-select
-                            :items="['0-17', '18-29', '30-54', '54+']"
-                            label="Age*"
-                            required
-                            ></v-select>
-                        </v-col>
-                        <v-col
-                            cols="12"
-                            sm="6"
-                        >
-                            <v-autocomplete
-                            :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                            label="Interests"
-                            multiple
-                            ></v-autocomplete>
-                        </v-col>
-                        </v-row>
-                    </v-container>
-                    <small>*indicates required field</small>
-                    </v-card-text>
-                    <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="dialog = false"
-                    >
-                        Close
-                    </v-btn>
-                    <v-btn
-                        color="blue darken-1"
-                        text
-                        @click="dialog = false"
-                    >
-                        Save
-                    </v-btn>
-                    </v-card-actions>
-                </v-card>
-                </v-dialog>
-            
+            <v-progress-circular v-if="loading" indeterminate color="primary"></v-progress-circular>
         </v-col>
     </v-row>
-    
+
     <v-row>
         <v-col class="ml-4 mr-4">
-            <v-data-table 
-            :height="700"
-            :dense="true"
-            :headers="profileHeader" 
-            :items="profiles" 
-            :items-per-page="20" 
-            @click:row="clickedRow"            
-            class="elevation-1"></v-data-table>
+            <v-data-table :height="700" :dense="true" :headers="profileHeader" :items="profiles" item-key="profid" :items-per-page="20" class="elevation-4" v-model="selected" :single-select="true">
+                <template slot="item" slot-scope="props">
+                    <tr @click="showAlert(props.item)">
+                        <td>{{ props.item.profid }}</td>
+                        <td>{{ props.item.accountname }}</td>
+                        <td>{{ props.item.address }}</td>
+                        <td>{{ props.item.gender.genderdescrip }}</td>
+                        <td>{{ props.item.proftype.proftypedescrip }}</td>
+                        <td>
+                            <v-btn slot="activator" small fab color="info" @click="dialog=true">
+                                <v-icon>edit</v-icon>
+                            </v-btn>
+                        </td>
+                    </tr>
+                </template>
+            </v-data-table>
         </v-col>
     </v-row>
-    
+    <v-layout row justify-center>
+        <v-dialog v-model="dialog" persistent max-width="600px" absolute>
+
+            <v-card>
+                <v-card-title>
+                    <h5>{{ selected.profid }} - {{ selected.accountname }}</h5>
+                    <br>
+                </v-card-title>
+                <v-card-text>
+                    <v-container grid-list-md>
+                        <v-layout wrap>
+
+                            <v-flex>
+                                <v-text-field label="Account Name*" required></v-text-field>
+                            </v-flex>
+
+                            <v-flex>
+                                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+                            </v-flex>
+
+                            <v-flex>
+                                <v-text-field label="Legal last name*" hint="example of persistent helper text" persistent-hint required></v-text-field>
+                            </v-flex>
+
+                            <v-flex xs12 sm6>
+                                <v-autocomplete :items="selected.genders" label="Gender" multiple></v-autocomplete>
+                            </v-flex>
+
+                        </v-layout>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" @click="dialog = false">Close</v-btn>
+                    <v-btn color="blue darken-1" @click="dialog = false">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+
+        </v-dialog>
+    </v-layout>
+
 </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: 'Home',
+    name: "Home",
     components: {
         //HelloWorld
     },
@@ -127,42 +88,91 @@ export default {
         console: () => console,
         window: () => window,
     },
+    watch: {
+        dialog(val) {
+            if (val) {
+                document.addEventListener('mousedown', this.mousedownHandler)
+            } else {
+                document.removeEventListener('mousedown', this.mousedownHandler)
+            }
+        }
+    },
     methods: {
-        clickedRow: (e) => {
-            console.log(e);
-            this.currentRow = e;
+        clickedRow: (item, row) => {
+            row.select(true);
+            console.log(item);
         },
-       
+        doubleClicked: (row) => {
+            console.log(row);
+        },
+        showAlert(a) {
+            console.log(this.selected);
 
+            this.selected.profid = a.profid;
+            this.selected.accountname = a.accountname;
+            this.selected.gender = a.gender;
+            this.selected.proftype = a.proftype;
+            this.selected.genders = [];
+
+            //to convert object to array
+            for (let gen in a.gender) {
+                this.selected.genders.push(gen);
+            }
+        }
     },
     mounted() {
         this.loading = true;
-          axios
-            .get('http://localhost:8000/api/profiles')
-            .then(response => (this.profiles = response.data))
-            this.loading = false;
+        axios
+            .get("http://localhost:8000/api/profiles")
+            .then((response) => (this.profiles = response.data));
+        this.loading = false;
     },
     data() {
         return {
+            name: "",
             loading: true,
             dialog: false,
-            currentRow: [],
-            profileHeader: [
-                {text: 'Profile ID', align: 'start', sortable: true, value: 'profid'},
-                {text: 'Account Name', align: 'start', sortable: true, value: 'accountname'},
-                {text: 'Address', align: 'start', sortable: true, value: 'address'},
-                {text: 'Gender', align: 'start', sortable: true, value: 'gender.genderdescrip'},
-                {text: 'Type', align: 'start', sortable: true, value: 'proftype.proftypedescrip'},
-            ],
             profiles: [],
-        }
+            selected: [],
+            profileHeader: [{
+                    text: "Profile ID",
+                    align: "start",
+                    sortable: true,
+                    value: "profid",
+                },
+                {
+                    text: "Account Name",
+                    align: "start",
+                    sortable: true,
+                    value: "accountname",
+                },
+                {
+                    text: "Address",
+                    align: "start",
+                    sortable: true,
+                    value: "address",
+                },
+                {
+                    text: "Gender",
+                    align: "start",
+                    sortable: true,
+                    value: "gender.genderdescrip",
+                },
+                {
+                    text: "Type",
+                    align: "start",
+                    sortable: true,
+                    value: "proftype.proftypedescrip",
+                },
+            ],
+
+        };
     },
-}
+};
 </script>
 
 <style>
-.v-data-table>.v-data-table__wrapper>table>tbody>tr>td 
-{
-  font-size: 10px;
+.v-data-table>.v-data-table__wrapper>table>tbody>tr>td {
+    font-size: 10px;
 }
 </style>
